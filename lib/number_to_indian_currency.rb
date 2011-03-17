0400306caaf7678f8a967246497fe7b20b912cff
Sandip Ransing <sandip@joshsoftware.com>
2011-03-18 01:55:57 +0530

    Font CSS

--- lib/number_to_indian_currency.rb
+++ lib/number_to_indian_currency.rb
@@ -1,7 +1,43 @@
 module NumberToIndianCurrency
-  def number_to_indian_currency(number, html=false)
-    number = number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")
-    "Rs.#{number}"
+  def self.initialize
+    return if @intialized
+    raise "ActionView is not available." unless defined?(ActionView)
+    ActionView::Base.send :include, CurrencyHelper
+    NumberToIndianCurrency.install
+    @intialized = true
+  end
+
+  def self.install
+    require 'fileutils'
+    orig = File.join(File.dirname(__FILE__), '..', 'public', 'stylesheets')
+    dest = File.join(Rails.root.to_s, 'public', 'stylesheets')
+    rupees_css = File.join(dest, 'rupees.css')
+    unless File.exists?(rupees_css) && FileUtils.identical?(File.join(orig, 'rupees.css'), rupees_css)
+      if File.exists?(rupees_css)
+        begin
+          puts "Removing..."
+          FileUtils.rm rupees_css
+          FileUtils.cp_r "#{orig}/.", dest
+        rescue Exception => e
+          puts e.message
+        end
+      else
+        begin
+          puts "Adding..."
+          FileUtils.cp_r "#{orig}/.", dest
+        rescue Exception => e
+          puts e.message
+        end
+      end
+    end
+  end
+
+  module CurrencyHelper
+    def number_to_indian_currency(number, html=false)
+      txt = html ? content_tag(:span, 'Rs.', :class => :WebRupee) : 'Rs.' "#{txt}
+      #{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}" 
+    end
   end
 end
-ActionView::Base.send :include, NumberToIndianCurrency
+
+NumberToIndianCurrency.initialize
---
+++ tasks/currency.rake
@@ -0,0 +1,5 @@
+namespace :number_to_indian_currency do
+  desc 'Copies font CSS to public directory'
+  task :install do
+  end
+end
