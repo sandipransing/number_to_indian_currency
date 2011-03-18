2d11b78446154063bc2d66eb63f43f9985c6fb0b
Sandip Ransing <sandip@joshsoftware.com>
2011-03-18 15:59:51 +0530

    Default WebRupee

--- lib/number_to_indian_currency.rb
+++ lib/number_to_indian_currency.rb
@@ -33,9 +33,9 @@ module NumberToIndianCurrency
   end
 
   module CurrencyHelper
-    def number_to_indian_currency(number, html=false)
-      txt = html ? content_tag(:span, 'Rs.', :class => :WebRupee) : 'Rs.' "#{txt}
-      #{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}" 
+    def number_to_indian_currency(number, html=true)
+      txt = html ? content_tag(:span, 'Rs.', :class => :WebRupee) : 'Rs.'
+      "#{txt} #{number.to_s.gsub(/(\d+?)(?=(\d\d)+(\d)(?!\d))(\.\d+)?/, "\\1,")}" 
     end
   end
 end
